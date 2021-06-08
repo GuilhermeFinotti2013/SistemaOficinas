72cfe800220e95cfacd32b4c75559e88d0623aa8
GuilhermeFinotti2013 <guilhermelfinotti@gmail.com>
2021-06-08 19:00:03 -0300

    Ewstrutura inicial

---
+++ src/SistemaOficinas.Data/ORM/OficinasDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaOficinas.Data.ORM
+{
+    public class OficinasDbContext : DbContext
+    {
+    }
+}
---
+++ src/SistemaOficinas.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SistemaOficinas.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
