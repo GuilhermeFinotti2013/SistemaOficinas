5d2928b7b08ba22dc779989252ea30bdd0bb5893
GuilhermeFinotti2013 <guilhermelfinotti@gmail.com>
2021-06-15 19:26:23 -0300

    Criado o repositorio de cliente. Cliando o projeto da camada de aplicação

---
+++ src/SistemaOficinas.Domain/Interfaces/Entidades/IClienteRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SistemaOficinas.Domain.Models;
+using SistemaOficinas.DomainCore.Base;
+
+namespace SistemaOficinas.Domain.Interfaces.Entidades
+{
+    public interface IClienteRepositorio : IRepositorioGenerico<Cliente, Guid>
+    {
+    }
+}
---
+++ src/SistemaOficinas.Repository/Entidades/ClienteRepositorio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SistemaOficinas.Repository.Base;
+using SistemaOficinas.Domain.Models;
+using SistemaOficinas.Data.ORM;
+
+namespace SistemaOficinas.Repository.Entidades
+{
+    public class ClienteRepositorio : RepositorioGenerico<Cliente, Guid>
+    {
+        private readonly OficinasDbContext _contexto;
+
+        public ClienteRepositorio(OficinasDbContext context) : base(context)
+        {
+            this._contexto = context;
+        }
+    }
+}
